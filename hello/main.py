375824a0e9baaedad9ce13e9d34dbf49ec17b8d0
tjctw <tsejen.tw@gmail.com>
2016-05-15 13:51:27 +0800

    add log to see waht happended..

--- hello/main.py
+++ hello/main.py
@@ -14,26 +14,62 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import logging
 import webapp2
+from valid import *
 form="""
 <form method="post">
     Whas is your birthday?
     <br>
-    <input type="text" name="month">
-    <input type="text" name="day">
-    <input type="text" name="year">
+    <label>
+        Month
+        <input type="text" name="month" value=""%(month)s"">
+    </label>
+    <label>
+        Day
+        <input type="text" name="day" value="%(day)s">
+    </label>
+    <label>
+        Year
+        <input type="text" name="year" value="%(year)s">
+    </label>
+
     <br>
+    <div style="color: red"> %(error)s</div>
     <br>
 <input type="submit"></input>
 </form>
 """
 class MainHandler(webapp2.RequestHandler):
+    def writeform(self, error="", year="", day="", month=""):
+        self.response.write(form % {"error":error, "month":month, "year":year, "day":day})
+
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write(form)
+        logging.debug('This is a debug message')
+        logging.info('This is an info message')
+        logging.warning('This is a warning message')
+        logging.error('This is an error message')
+        logging.critical('This is a critical message')
+        self.writeform()
 
     def post(self):
-        self.response.write("Thanks, that might be a good day to born.")
+        user_month  = self.request.get('month')
+        user_day    = self.request.get('day')
+        user_year   = self.request.get('year')
+        month = valid_month(user_month)
+        day = valid_month(user_day)
+        year = valid_month(user_year)
+
+        if not (day and month and year):
+            self.writeform("Hey wtf are you entering!!(badass server)")
+            self.response.write(user_day)
+            self.response.write(user_month)
+            self.response.write(user_year)
+        else:
+            self.response.writeform("Thanks, that might be a good day to born.")
+
+
 
 class TestHandler(webapp2.RequestHandler):
     def post(self):
---
+++ hello/valid.py
@@ -0,0 +1,132 @@
+# -----------
+# User Instructions
+#
+# Modify the valid_month() function to verify
+# whether the data a user enters is a valid
+# month. If the passed in parameter 'month'
+# is not a valid month, return None.
+# If 'month' is a valid month, then return
+# the name of the month with the first letter
+# capitalized.
+#
+from __future__ import print_function
+from valid import *
+
+months = ['January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December']
+
+month_abbvs = dict((m[:3].lower(),m) for m in months)
+
+def valid_month(month):
+    """
+    Simply use https://docs.python.org/2/library/string.html#string.capitalize
+    >>> valid_month("january")
+    'January'
+    >>> valid_month("foo") is None
+    True
+    >>> valid_month("Dec")
+    'December'
+    >>> valid_month("dec")
+    'December'
+    """
+    return month_abbvs.get(month[:3].lower()) # pretty neat!
+
+
+
+
+# -----------
+# User Instructions
+#
+# Modify the valid_day() function to verify
+# whether the string a user enters is a valid
+# day. The valid_day() function takes as
+# input a String, and returns either a valid
+# Int or None. If the passed in String is
+# not a valid day, return None.
+# If it is a valid day, then return
+# the day as an Int, not a String. Don't
+# worry about months of different length.
+# Assume a day is valid if it is a number
+# between 1 and 31.
+# Be careful, the input can be any string
+# at all, you don't have any guarantees
+# that the user will input a sensible
+# day.
+#
+# Hint: The string function isdigit() might be helpful.
+
+def valid_day(day):
+    """
+    >>> valid_day('0') is None
+    True
+    >>> valid_day('-11') is None
+    True
+    >>> valid_day('foo') is None
+    True
+    >>> valid_day(10)
+    10
+    """
+    if type(day) is str:
+        if day.isdigit():
+            day = int(day)
+    if day and type(day) is int:
+        return  day if day<32 and day>0 else None
+    else:
+        return None
+
+
+
+
+
+
+# -----------
+# User Instructions
+#
+# Modify the valid_year() function to verify
+# whether the string a user enters is a valid
+# year. If the passed in parameter 'year'
+# is not a valid year, return None.
+# If 'year' is a valid year, then return
+# the year as a number. Assume a year
+# is valid if it is a number between 1900 and
+# 2020.
+#
+
+
+def valid_year(year):
+    """
+    >>> valid_year('0') is None
+    True
+    >>> valid_year('-11') is None
+    True
+    >>> valid_year('1950')
+    1950
+    >>> valid_year('2000')
+    2000
+    >>> valid_year(1999)
+    1999
+    >>> valid_year('foo') is None
+    True
+    >>> valid_year('10') is None
+    True
+    """
+    if year:
+        if type(year) is str:
+            year = int(year) if year.isdigit() else None
+        return year if 2020 > year > 1900 else None
+
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
