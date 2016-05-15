12db024fe4ea462c18f63bcb22ee889ac232a4b9
tjctw <tsejen.tw@gmail.com>
2016-05-15 11:55:37 +0800

    use dict to map variable

--- hello/replace_string.py
+++ hello/replace_string.py
@@ -38,3 +38,21 @@ print sub2("running", "sleeping")
 # => "I think running and sleeping are perfectly normal things to do in public."
 print sub2("sleeping", "running")
 # => "I think sleeping and running are perfectly normal things to do in public."
+
+
+# User Instructions
+#
+# Write a function 'sub_m' that takes a
+# name and a nickname, and returns a
+# string of the following format:
+# "I'm NICKNAME. My real name is NAME, but my friends call me NICKNAME."
+#
+
+given_string2 = "I'm %(nickname)s. My real name is %(name)s, but my friends call me %(nickname)s."
+def sub_m(name, nickname):
+    return given_string2 %{"nickname":nickname, "name":name}
+
+
+
+print sub_m("Mike", "Goose")
+# => "I'm Goose. My real name is Mike, but my friends call me Goose."
